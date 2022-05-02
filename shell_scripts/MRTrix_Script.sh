ff4332a36b9cebce186bacb9e97e220f09dd7c12
Ana Matoso <anamatoso92@gmail.com>
2022-05-02 15:05:21 +0100

    Update MRTrix_Script.sh

--- shell_scripts/MRTrix_Script.sh
+++ shell_scripts/MRTrix_Script.sh
@@ -3,36 +3,35 @@
 # Have to be in data directory
 MAINDIR=$(pwd)
 display_usage() {
-	echo "$(basename $0) [Subject] [type of session]"
+	echo "$(basename $0) [Subject and type of session]"
 	# echo "This script uses MRTrix to analyze diffusion data. It requires X arguments: 
 	# 	1) Subject DWI Directory. Example: sub-control019
 	#	2) Type of session. Example: ses-midcycle
 	}
 
-	if [ $# -le 1 ] # if there are 1 argument or less
+	if [ $# -le 0 ] # if there are 1 argument or less
 	then
 		display_usage
 		exit 1
 	fi
 
-
-SUB=$1 #example name: sub-control019
-SES=$2
+DIR=$1 #example name: sub-control019_ses-midcycle
+SUB=${place:0:14} # example name: sub-control019
 
 ########################### STEP 1 ###################################
 #             		  Prepare data and directories					 #
 ######################################################################
 
-SUBDIR="${MAINDIR}/${SUB}_${SES}" #example name: sub-control019_ses-midcycle
+SUBDIR="${MAINDIR}/${DIR}" 
 ANATDIR="${MAINDIR}/${SUB}" #example name: sub-control019
-ANAT="${MAINDIR}/${SUB}/${SUB}_restored-MPRAGE_brain.nii.gz"
+ANAT="${ANATDIR}/${SUB}_restored-MPRAGE_brain.nii.gz"
 cd $SUBDIR
 
 # WHEN DOING FOR REAL, CHECK IF THERE IS MORE DATA WITH MORE BVALUES
-DWI="${MAINDIR}/${SUB}_${SES}/*b0b1000b2000.nii.gz"
-DWIMASK="${MAINDIR}/${SUB}_${SES}/*clean_mask.nii.gz"
-BVEC="${MAINDIR}/${SUB}_${SES}/*b0b1000b2000_bvecs.bvec"
-BVAL="${MAINDIR}/${SUB}_${SES}/*b0b1000b2000_bvals.bval"
+DWI="${SUBDIR}/*b0b1000b2000.nii.gz"
+DWIMASK="${SUBDIR}/*clean_mask.nii.gz"
+BVEC="${SUBDIR}/*b0b1000b2000_bvecs.bvec"
+BVAL="${SUBDIR}/*b0b1000b2000_bvals.bval"
 
 mkdir -p mrtrix_outputs
 cd mrtrix_outputs
@@ -105,12 +104,18 @@ mrcalc atlas_coreg.mif -round -datatype uint32 atlas.mif -force
 # Restric the GM/WM boundary to the atlas
 mrcalc atlas.mif gmwmSeed_coreg.mif -mult gmwmseed_atlas.mif -force
 
+# Create file with sizes of ROIs (in voxels)
+mrconvert atlas.mif atlas.nii.gz -force
+fslstats atlas.nii.gz -h 117 >> roi_size_w0.txt
+tail -n +2 roi_size_w0.txt >> roi_size.txt
+rm roi_size_w0.txt
+
 ########################### STEP 6 ###################################
 #                 Run the streamline analysis                        #
 ######################################################################
 
-# Create streamlines: maxlength=250mm, 1M seeds
-tckgen -act 5tt_coreg.mif -algorithm SD_STREAM -seed_gmwmi gmwmseed_atlas.mif -maxlength 250 -select 1000000 wmfod_norm.mif tracks.tck -force
+# Create streamlines: maxlength=250mm, 10M seeds
+tckgen -act 5tt_coreg.mif -algorithm SD_STREAM -seed_gmwmi gmwmseed_atlas.mif -maxlength 250 -select 10000000 wmfod_norm.mif tracks.tck -force
 
 # Reduce the number of streamlines with tcksift
 tcksift2 -act 5tt_coreg.mif -out_mu sift_mu.txt -out_coeffs sift_coeffs.txt tracks.tck wmfod_norm.mif sift.txt -force
@@ -120,6 +125,6 @@ tcksift2 -act 5tt_coreg.mif -out_mu sift_mu.txt -out_coeffs sift_coeffs.txt trac
 ######################################################################
 
 #Creating the connectome 
-tck2connectome -symmetric -zero_diagonal -scale_invnodevol -tck_weights_in sift.txt tracks.tck atlas.mif "${SUB}_${SES}_connmatrix.csv" -force
+tck2connectome -symmetric -zero_diagonal -scale_invnodevol -tck_weights_in sift.txt tracks.tck atlas.mif "${DIR}_mrtrix_matrix.csv" -force
 
 cd $MAINDIR
