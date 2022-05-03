9298dd70da91d1ab9d097769237fd401c8b61b82
Ana Matoso <anamatoso92@gmail.com>
2022-05-03 12:41:37 +0100

    Update main.m

--- matlab_scripts/main.m
+++ matlab_scripts/main.m
@@ -1,122 +1,59 @@
-%% Load data
-connectomes=load_data('/Users/ana/Documents/Ana/universidade/Tese/Code/matrices data','*.csv'); 
-% size= 116 x 116 x n_people
+%% Load data from matrices
+% directory where connectivity matrices are
+dir='/Users/ana/Documents/Ana/universidade/Tese/Code/matlab_scripts/mat_data';
 
-%% Import data
-connectome1k=importdata("__connmatrix1000.csv");
-connectome5k=importdata("__connmatrix5000.csv");
-connectome10k=importdata("__connmatrix10000.csv");
-connectome50k=importdata("__connmatrix50000.csv");
-connectome100k=importdata("__connmatrix100000.csv");
-connectome500k=importdata("__connmatrix500000.csv");
-connectome1M=importdata("__connmatrix1000000.csv");
-connectome5M=importdata("__connmatrix5000000.csv");
-connectome10M=importdata("__connmatrix10000000.csv");
-%% Comparison of connectivity matrices mrtrix probablistic
-f=figure();
-subplot(2,3,1)
-plotconmat("connmatrix5k.csv",5000,"5k probablistic")
+% Controls midcyle
+HC_midcycle_mrtrix=load_data(dir,'*midcycle*mrtrix*'); %116 x 116 x n_people
+HC_midcycle_fsl=load_data(dir,'*midcycle*fsl*'); 
 
-subplot(2,3,2)
-plotconmat("connmatrix10k.csv",10000,"10k probablistic")
+% Patients interictal
+M_interictal_mrtrix=load_data(dir,'*interictal*mrtrix*');
+M_interictal_fsl=load_data(dir,'*interictal*fsl*');
 
-subplot(2,3,3)
-plotconmat("connmatrix50k.csv",50000,"50k probablistic")
+connectomes={HC_midcycle_mrtrix HC_midcycle_fsl M_interictal_mrtrix M_interictal_fsl};
 
-subplot(2,3,4)
-plotconmat("connmatrix100k.csv",100000,"100k probablistic")
-
-subplot(2,3,5)
-plotconmat("connmatrix.csv",1000000,"1M probablistic")
-
-subplot(2,3,6)
-plotconmat("__connmatrix1000000.csv",1000000,"1M deterministic")
-
-sgtitle ("Comparison of matrices of mrtrix probablistic",'interpreter',...
-    'latex','FontUnits','points','FontWeight','demi','FontSize',18,'FontName','Times')
-f.WindowState = 'maximized';
-
-%% Comparison of connectivity matrices mrtrix deterministic
-f=figure();
-
-subplot(2,4,1)
-plotconmat("__connmatrix5000.csv",5000,"5k")
-
-subplot(2,4,2)
-plotconmat("__connmatrix10000.csv",10000,"10k")
-
-subplot(2,4,3)
-plotconmat("__connmatrix50000.csv",50000,"50k")
-
-subplot(2,4,4)
-plotconmat("__connmatrix100000.csv",100000,"100k")
-
-subplot(2,4,5)
-plotconmat("__connmatrix500000.csv",500000,"500k")
-
-subplot(2,4,6)
-plotconmat("__connmatrix1000000.csv",1000000,"1M")
-
-subplot(2,4,7)
-plotconmat("__connmatrix5000000.csv",5000000,"5M")
-
-subplot(2,4,8)
-plotconmat("__connmatrix10000000.csv",10000000,"10M")
-
-sgtitle ("Comparison of matrices of mrtrix deterministic",'interpreter',...
-    'latex','FontUnits','points','FontWeight','demi','FontSize',18,'FontName','Times')
-
-f.WindowState = 'maximized';
-
-%% Evolution of certain ROIxROI connectivity
-f=figure();
-for j=1:10
-    subplot(2,5,j)
-    i=1;
-    while i<=10
-        n=randi(116);m=randi(116);if m==n,m=randi(116); end
-        a=connectome10M(n,m)./1e7;
-        value=[connectome1k(n,m)./1e3 connectome5k(n,m)./5e3 connectome10k(n,m)./1e4 ...
-            connectome50k(n,m)./5e4 connectome100k(n,m)./1e5 connectome500k(n,m)./5e5 ...
-            connectome1M(n,m)./1e6 connectome5M(n,m)./5e6 connectome10M(n,m)./1e7];
-        %value=value./a;
-        if ~(any(value(:) == 0))
-            i=i+1;
-            nstreamlines=[1000 5000 10000 50000 100000 500000 1000000 5000000 10000000];
-            semilogx(nstreamlines,value,'DisplayName',"ROIS "+num2str(min(n,m))+" X "+num2str(max(n,m)));
-            xlabel ('Number of streamlines')
-            xlim([1000 1e7])
-            ylabel ('Connectivity')
-            hold on
-        end
-        
-    end
-    legend('show')
-    legend('Location','best')
+% Calculate people per situation
+n_people=zeros(1,length(connectomes));
+for i=1:length(connectomes)
+    conmat=connectomes{i};
+    s=size(conmat);
+    n_people(i)=s(end);
 end
-sgtitle('Connectivity Convergence','interpreter','latex','FontUnits','points',...
-    'FontWeight','demi','FontSize',18,'FontName','Times')
-f.WindowState = 'maximized';
 
-%% FSL vs MRTrix (det) 5k
+clear dir s conmat i
+%% Test for spurious connections
 
-connectomeFSL=normalize_roisize("fdt_network_matrix","roi_size.txt");
+significance_mask=zeros(116,116,length(connectomes));
+for i=1:length(connectomes)
+    significance_mask(:,:,i) = signtest_mask(connectomes(i));    
+end
+% imagesc(significance_mask(i)); colormap jet
+%% Calculate metrics
 
-%Plot Figure
-figure()
+for i=1:length(connectomes)
+    conmats=connectomes{i};
+    for p=1:n_people(i)
+        mat=conmats(p);                 % connectivity matrix
+        len_mat=1./mat;                 % conection-length matrix
+        d_mat= distance_wei(len_mat);   % distance matrix
+        
+        %calculate metrics
+        BC=betweenness_wei(len_mat);                                % betweenness centrality
+        [L,GE]=charpath(d_mat);                                         % characteristic path length and global efficiency
+        Ci=clustering_coef_wu(weight_conversion(mat, 'normalize')); % local clustering coefficient
+        C=mean(Ci);                                                 % global clustering coefficient
+        EC=eigenvector_centrality_und(mat);                         % eigenvector centrality
+        [Qi, Q] = modularity_und(mat);                              % modularity
+        RC=rich_club_wu(mat);                                          % rich club coefficient
+        %degree=strengths_und(mat);                                  % node strength
+        T=transitivity_wu(mat);                                     % transitivity
+        S=smallworldness(C,L,mat);                                  %smallworldness
+    
+        % insert metrics in matrix
+        metrics{i,p}=[BC L GE C EC Q RC T S];     
+    end
+end
 
-subplot(1,2,1)
-plotconmat(connectomeFSL,5000,"FSL 5k")
+%% Analysis of results
 
-subplot(1,2,2)
-plotconmat("__connmatrix5000.csv",5000,"MRTrix 5k (Deterministic)")
 
-sgtitle ("Comparison of 5k streamlines in FSL and MRTrix",'interpreter',...
-    'latex','FontUnits','points','FontWeight','demi','FontSize',18,'FontName','Times')
-
-%% See sifting coefficients
-fileID=fopen('sift_coeffs.txt','r');
-formatSpec = '%f';
-A = fscanf(fileID,formatSpec);
-histogram(A,'Normalization','probability')
-title('sift coefficient vs probability')
