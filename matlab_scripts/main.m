074f5ae738041d5a3215147a5b4c3605d3349ab2
Ana Matoso <anamatoso92@gmail.com>
2022-05-08 20:03:17 +0100

    Update main.m

--- matlab_scripts/main.m
+++ matlab_scripts/main.m
@@ -1,17 +1,18 @@
 %% Load data from matrices
+format long
 % directory where connectivity matrices are
 dir='/Users/ana/Documents/Ana/universidade/Tese/Code/matlab_scripts/matrix_data';
 
 % Controls midcyle
-HC_midcycle_mrtrix=load_data(dir,'*midcycle*mrtrix*'); %116 x 116 x n_people
-%HC_midcycle_fsl=load_data(dir,'*midcycle*fsl*'); 
+HC_midcycle_mrtrix=load_data(dir,'*midcycle*mrtrix*bval2.csv'); %116 x 116 x n_people
+%HC_midcycle_fsl=load_data(dir,'*midcycle*fsl*');
 
 % Patients interictal
-M_interictal_mrtrix=load_data(dir,'*interictal*mrtrix*');
+M_interictal_mrtrix=load_data(dir,'*interictal*mrtrix*bval2.csv');
 %M_interictal_fsl=load_data(dir,'*interictal*fsl*');
 
 % Note: they are not normalized by number of streamlines
-connectomes={HC_midcycle_mrtrix M_interictal_mrtrix}; 
+connectomes={HC_midcycle_mrtrix M_interictal_mrtrix};
 n_conditions=length(connectomes);
 
 % Calculate people per situation
@@ -41,36 +42,92 @@ load("metrics.mat")
 for i=1:n_conditions
     conmats=connectomes{i};
     for p=1:n_people(i)
-        mat=conmats(:,:,p);                 % connectivity matrix
-        %m(:,p)=calculate_metrics(mat);
-        m2(:,p)=calculate_metrics_v2(mat);
+        mat=conmats(:,:,p); % connectivity matrix
+        m(:,p)=calculate_metrics(mat);
+        %m2(:,p)=calculate_metrics_v2(mat);
     end
-    %metrics{i}=m;
-    metrics2{i}=m2;
+    metrics{i}=m;
+    %metrics2{i}=m2;
 end
 clear i p mat conmats m m2
 %% Analysis of results
-[n_metrics,~]=size(metrics2{1});
+[n_metrics,~]=size(metrics{1});
 metrics_labels=get_label_metrics();
 
 metrics_sign=[];
+
 for m=1:n_metrics
-    hc=metrics2{1}(m,:);hc=hc(~isnan(hc));
-    mig=metrics2{2}(m,:);mig=mig(~isnan(mig));
+    hc=metrics{1}(m,:);hc=hc(~isnan(hc));
+    mig=metrics{2}(m,:);mig=mig(~isnan(mig));
+    
     if (isequal(hc,zeros(1,length(hc))) && isequal(mig,zeros(1,length(mig)))) || isempty(hc) || isempty(mig)
         continue
     end
     x = [hc mig];
-    g = [zeros(1,n_people(1)),ones(1,n_people(2))];
+    g = [zeros(1,length(hc)),ones(1,length(mig))];
     
-     if ttest2(hc,mig, 'Alpha', 0.05/4) || ttest2(hc,mig, 'Alpha', 0.05/4,'Tail','right') || ttest2(hc,mig, 'Alpha', 0.05/4,'Tail','left')
-%         figure
-%         boxplot(x,g,'Labels',{'HC','M'})
-%         hold on
-%         text(2.1,1.01*quantile(mig,0.75), '*','FontSize',14,'Color','red');
-%         hold off
+    if ttest2(hc,mig)
         metrics_sign=[metrics_sign m];
+        
+        %figure
+        %boxplot(x,g,'Labels',{'HC','M'})
+        %hold on
+        %text(2.1,1.01*quantile(mig,0.75), '*','FontSize',14,'Color','red');
+        %hold off
     end
+    
     
 end
 disp(metrics_labels(metrics_sign))
+%% Visualization of results
+
+metrics_mean=mean_met(metrics);
+
+% Rich club coefficient
+figure;
+for i=1:n_conditions
+    met=metrics{i};
+    switch i
+        case 1
+            color='b';
+        case 2
+            color='r';
+    end
+    for p=1:n_people(i)
+        plot(linspace(1,115,115),met(469:583,p),color)
+        title("Rich club coefficient curve")
+        xlabel("Degree")
+        ylabel("Rich club coefficient")
+        hold on
+    end
+end
+
+% L, GE, C, Q , T, S
+idx=[117 118 119 468 700 701];
+for i=1:length(idx)
+    index=idx(i);
+    figure;
+    
+    hc=metrics{1}(index,:);hc=hc(~isnan(hc));
+    mig=metrics{2}(index,:);mig=mig(~isnan(mig));
+    
+    x = [hc mig];
+    g = [zeros(1,length(hc)),ones(1,length(mig))];
+    
+    boxplot(x,g,'Labels',{'HC','M'})
+    title(metrics_labels(index))
+    ylabel(metrics_labels(index))
+   
+    if ttest2(hc,mig)
+        hold on
+        text(2.1,1.01*quantile(mig,0.75), '*','FontSize',25);
+        hold off
+    end
+end
+
+
+
+
+
+
+
