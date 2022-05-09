601fea7013c86bdb78e3d3495711810e0279585d
Ana Matoso <anamatoso92@gmail.com>
2022-05-09 17:42:48 +0100

    Update get_label_metrics.m

--- matlab_scripts/get_label_metrics.m
+++ matlab_scripts/get_label_metrics.m
@@ -6,24 +6,25 @@ EC=strings(1,116);
 Qi=strings(1,116);
 D=strings(1,116);
 RC=strings(1,115);
+node_labels=get_label_nodes("AAL116_labels.txt");
 for i=1:116
     if i<=115
-        BC(i)="BC_"+num2str(i);
-        Ci(i)="Ci_"+num2str(i);
-        EC(i)="EC_"+num2str(i);
-        Qi(i)="Qi_"+num2str(i);
-        D(i)="D_"+num2str(i);
-        RC(i)="RC_"+num2str(i);
+        BC(i)="BC_"+node_labels(i);
+        Ci(i)="Ci_"+node_labels(i);
+        EC(i)="EC_"+node_labels(i);
+        Qi(i)="Qi_"+node_labels(i);
+        D(i)="D_"+node_labels(i);
+        RC(i)="RC_"+node_labels(i);
         
     else
-        BC(i)="BC_"+num2str(i);
-        Ci(i)="Ci_"+num2str(i);
-        EC(i)="EC_"+num2str(i);
-        Qi(i)="Qi_"+num2str(i);
-        D(i)="D_"+num2str(i);
+        BC(i)="BC_"+node_labels(i);
+        Ci(i)="Ci_"+node_labels(i);
+        EC(i)="EC_"+node_labels(i);
+        Qi(i)="Qi_"+node_labels(i);
+        D(i)="D_"+node_labels(i);
     end
 end
 
-metrics_labels=[BC "L" "GE" "C" Ci EC Qi "Q" RC D "T" "S"];
+metrics_labels=[BC "L" "GE" "C" Ci EC Qi "Q" RC D "T" "S" "A"];
 end
 
