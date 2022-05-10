c0b5a1c415ac696ea33f2b4b8cd91eee9991aff4
Ana Matoso <anamatoso92@gmail.com>
2022-05-10 13:31:14 +0100

    Update calculate_metrics.m

--- matlab_scripts/calculate_metrics.m
+++ matlab_scripts/calculate_metrics.m
@@ -5,17 +5,18 @@ len_mat=1./mat;                 % conection-length matrix
 d_mat= distance_wei(len_mat);   % distance matrix
 
 %calculate metrics
-BC=betweenness_wei(len_mat)';                                   % betweenness centrality 1-116
-[L,GE]=charpath(d_mat);                                         % characteristic path length and global efficiency 117,118
-Ci=clustering_coef_wu(weight_conversion(mat, 'normalize'))';    % local clustering coefficient 120-235
-C=mean(Ci);                                                     % global clustering coefficient 119
-EC=eigenvector_centrality_und(mat)';                            % eigenvector centrality 236-351
-[Qi, Q]=modularity_und(mat);Qi=Qi';                             % modularity 352-467, 468
-RC=rich_club_wu_norm(mat,115);                                  % rich club coefficient 469-583
-degree=strengths_und(mat);                                      % node strength 584-699
-T=transitivity_wu(mat);                                         % transitivity 700
-S=smallworldness(mat);                                          % smallworldness 701
-A=assortativity_wei(mat,0);
+BC=betweenness_wei(len_mat)';                               % betweenness centrality 1-116
+[L,GE]=charpath(d_mat,0,0);                                     % characteristic path length and global efficiency 117,118
+Ci=clustering_coef_wu(weight_conversion(mat, 'normalize'))';% local clustering coefficient 120-235
+C=mean(Ci);                                                 % global clustering coefficient 119
+EC=eigenvector_centrality_und(mat)';                        % eigenvector centrality 236-351
+[Qi, Q]=modularity_und(mat);Qi=Qi';                         % modularity 352-467, 468
+RC=rich_club_wu_norm(mat,115);                              % rich club coefficient 469-583
+degree=strengths_und(mat);                                  % node strength 584-699
+T=transitivity_wu(mat);                                     % transitivity 700
+S=smallworldness(mat);                                      % smallworldness 701
+A=assortativity_wei(mat,0);                                 % assortivity 702
+
 % insert metrics in matrix
 metrics=[BC L GE C Ci EC Qi Q RC degree T S A]';
 end
