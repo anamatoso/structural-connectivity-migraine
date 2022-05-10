d8005040473f948bf14a5b172ecbdc87de368302
Ana Matoso <anamatoso92@gmail.com>
2022-05-10 13:31:34 +0100

    Update main.m

--- matlab_scripts/main.m
+++ matlab_scripts/main.m
@@ -7,12 +7,20 @@ dir='/Users/ana/Documents/Ana/universidade/Tese/Code/matlab_scripts/matrix_data'
 HC_midcycle_mrtrix=load_data(dir,'*midcycle*mrtrix*bval2.csv'); %116 x 116 x n_people
 %HC_midcycle_fsl=load_data(dir,'*midcycle*fsl*');
 
+% Controls premenstrual
+HC_premenstrual_mrtrix=load_data(dir,'*premenstrual*mrtrix*bval2.csv'); %116 x 116 x n_people
+%HC_midcycle_fsl=load_data(dir,'*premenstrual*fsl*');
+
 % Patients interictal
 M_interictal_mrtrix=load_data(dir,'*interictal*mrtrix*bval2.csv');
 %M_interictal_fsl=load_data(dir,'*interictal*fsl*');
 
+% Patients ictal
+M_ictal_mrtrix=load_data(dir,'*-ictal*mrtrix*bval2.csv');
+%M_ictal_fsl=load_data(dir,'*ictal*fsl*');
+
 % Note: they are not normalized by number of streamlines
-connectomes={HC_midcycle_mrtrix M_interictal_mrtrix};
+connectomes={HC_midcycle_mrtrix M_interictal_mrtrix HC_premenstrual_mrtrix M_ictal_mrtrix};
 n_conditions=length(connectomes);
 
 % Calculate people per situation
@@ -39,15 +47,14 @@ clear i p
 %load("metrics.mat")
 %% Calculate metrics
 % 24s per person
+clear metrics
 for i=1:n_conditions
     conmats=connectomes{i};
     for p=1:n_people(i)
         mat=conmats(:,:,p); % connectivity matrix
         m(:,p)=calculate_metrics(mat);
-        %m2(:,p)=calculate_metrics_v2(mat);
     end
     metrics{i}=m;
-    %metrics2{i}=m2;
 end
 clear i p mat conmats m m2
 %% Analysis of results
@@ -95,28 +102,27 @@ for i=1:n_conditions
             color='b';
         case 2
             color='r';
+        case 3
+            color='c';
+        case 4
+            color='y';
     end
     
- 
-    %for p=1:n_people(i)
-    %   plot(linspace(1,115,115),met(469:583,p),color)
-    
-    % plot means
     plot(linspace(1,115,115),mean(met(469:583,:),2),color)
     title("Rich club coefficient curve")
     xlabel("Degree")
     ylabel("Rich club coefficient")
     hold on
-    %end
 end
+
 % plot significance
 for m=469:583
     hc=metrics{1}(m,:);
     mig=metrics{2}(m,:);
     
-    if (isequal(hc,zeros(1,length(hc))) && isequal(mig,zeros(1,length(mig)))) || isempty(hc) || isempty(mig)
-        continue
-    end
+%     if (isequal(hc,zeros(1,length(hc))) && isequal(mig,zeros(1,length(mig)))) || isempty(hc) || isempty(mig)
+%         continue
+%     end
     
     if ttest2(hc,mig)==1
         plot(m-468,1,"+k")
@@ -125,8 +131,8 @@ for m=469:583
 end
 hold off
 
-
-%% Visualization of results - Plot General metrics L, GE, C, Q , T, S,A
+clear m hc mig i met color
+%% Visualization of results - Plot General metrics L, GE, C, Q, T, S,A
 idx=[117 118 119 468 700 701 702];
 for i=1:length(idx)
     index=idx(i);
@@ -142,26 +148,25 @@ for i=1:length(idx)
     title(metrics_labels(index))
     ylabel(metrics_labels(index))
     
-    if ttest2(hc,mig)
+    if ttest2(hc,mig)==1
         hold on
-        
-        hold off
+        text(2.1,1.01*quantile(mig,0.75), '*','FontSize',15,'Color','black');
     end
+    hold off
 end
 
+clear i idx index hc mig x g
 
-
-
-clear i met p idx hc mig x g color
-
-%% Visualization of results - Degree
+%% Visualization of results - Node Strength
 figure;
 x=["HC M"];
-bar(metrics_mean(584:699,:)); hold on
-title("Node Strength")
-xlabel("Condition")
-ylabel("Strength")
 
+title("Node Strength")
+xlabel("Node")
+ylabel("Strength")
+n_index=[];
+node=[];
+bar(metrics_mean(642:688,:)); hold on
 %plot significance
 for m=584:699
     hc=metrics{1}(m,:);
@@ -172,10 +177,35 @@ for m=584:699
     end
     
     if ttest2(hc,mig)==1
-        text(m-583,max(mean(hc),mean(mig))+0.1, '*','FontSize',15);
-        
-        hold on
+        text(m-583,max(mean(hc),mean(mig))+0.1, '*','FontSize',15); hold on
+        n_index=[n_index m];
+        node=[node m-583];
     end
 end
-hold off
 
+%xticks([59 60 61 62 67 68 69 70 77 78 83 89 90 92 93 94 95 96 98 99 100 101 102 103 105])
+hold off
+% legend({'HC', 'M'})
+% legend('Location','best')
+
+%% Analysis of results - ANOVA - General metrics L, GE, C, Q, T, S,A
+ms=[];
+idx=[117 118 119 468 700 701 702];
+for i=1:length(idx)
+    m=idx(i);
+    hc_mid=metrics{1}(m,:);
+    mig_inter=metrics{2}(m,:);
+    hc_pre=metrics{3}(m,:);
+    mig_ict=metrics{4}(m,:);
+
+    x = [hc_mid mig_inter hc_pre mig_ict];
+    g = [zeros(1,length(hc_mid)),ones(1,length(mig_inter)),2*ones(1,length(hc_pre)),3*ones(1,length(mig_ict))];
+    
+    [p, tbl, stats] = anova1(x,g);
+    if p<0.05
+        ms=[ms m];
+    end
+    
+end
+disp(metrics_labels(ms))
+clear i idx m hc_mid mig_inter hc_pre mig_ict x g p
