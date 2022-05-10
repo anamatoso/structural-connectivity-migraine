525fd0ad515cfbd437e5aec8fda0c13fd5690d11
Ana Matoso <anamatoso92@gmail.com>
2022-05-10 13:31:24 +0100

    Update smallworldness.m

--- matlab_scripts/smallworldness.m
+++ matlab_scripts/smallworldness.m
@@ -8,13 +8,13 @@ mat=round(mat.*1e30);
 len_mat=1./mat;                 % conection-length matrix
 d_mat= distance_wei(len_mat);
 C=mean(clustering_coef_wu(weight_conversion(mat, 'normalize'))); % clustering coefficient
-[L,~]=charpath(d_mat);
+[L,~]=charpath(d_mat,0,0);
 
 random_mat=randmio_und(mat,100);
 len_mat_rand=1./random_mat;                 % conection-length matrix
 d_mat_rand= distance_wei(len_mat_rand);
 Crand=mean(clustering_coef_wu(weight_conversion(random_mat, 'normalize'))); % clustering coefficient
-[Lrand,~]=charpath(d_mat_rand);
+[Lrand,~]=charpath(d_mat_rand,0,0);
 
 n=C/Crand;
 d=L/Lrand;
