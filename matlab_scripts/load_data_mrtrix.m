6a463ef1c1aea2ecaf7565fa651b2fc970169398
Ana Matoso <anamatoso92@gmail.com>
2022-05-11 17:25:18 +0100

    correct load datas to include normalization of sizes of roi

--- matlab_scripts/load_data_mrtrix.m
+++ matlab_scripts/load_data_mrtrix.m
@@ -1,4 +1,4 @@
-function [connectomes] = load_data(folder,pattern)
+function [connectomes] = load_data_mrtrix(folder,pattern)
 % This function load the data from a given folder which have a given
 % pattern
 
@@ -9,8 +9,6 @@ connectomes=zeros(116,116,length(theFiles));
 for k = 1 : length(theFiles)
     baseFileName = theFiles(k).name;
     fullFileName = fullfile(theFiles(k).folder, baseFileName);
-    % Now do whatever you want with this file name,
-    % such as reading it in as an image array with imread()
     c=importdata(fullFileName);
     connectomes(:,:,k)=c;
 end
