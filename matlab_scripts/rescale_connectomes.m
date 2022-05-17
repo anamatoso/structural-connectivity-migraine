a15a444d3a6023fd35edd10c0a83f925d824a7f3
Ana Matoso <anamatoso92@gmail.com>
2022-05-17 18:13:07 +0100

    Create rescale_connectomes.m

---
+++ matlab_scripts/rescale_connectomes.m
@@ -0,0 +1,15 @@
+function [c] = rescale_connectomes(connectomes,npeople)
+c=cell(size(connectomes));
+for i=1:length(connectomes)
+   matrices=connectomes{i};
+   newmat=zeros(size(matrices));
+   for p=1:npeople(i)
+    newmat(:,:,p)=rescale(matrices(:,:,p),0,1);
+   end
+   c{i}=newmat;
+end
+
+
+
+end
+
