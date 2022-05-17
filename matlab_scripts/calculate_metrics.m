2275364878e1d83b28cd81320fa6d97c8f0be0f3
Ana Matoso <anamatoso92@gmail.com>
2022-05-17 18:12:56 +0100

    Update calculate_metrics.m

--- matlab_scripts/calculate_metrics.m
+++ matlab_scripts/calculate_metrics.m
@@ -12,12 +12,13 @@ C=mean(Ci);                                                 % global clustering 
 EC=eigenvector_centrality_und(mat)';                        % eigenvector centrality 236-351
 [Qi, Q]=modularity_und(mat);Qi=Qi';                         % modularity 352-467, 468
 RC=rich_club_wu_norm(mat,115);                              % rich club coefficient 469-583
-degree=strengths_und(mat);                                  % node strength 584-699
-T=transitivity_wu(mat);                                     % transitivity 700
-S=smallworldness(mat);                                      % smallworldness 701
-A=assortativity_wei(mat,0);                                 % assortivity 702
+strength=strengths_und(mat);                                % node strength 584-699
+mean_strength=mean(strength);                               % mean strength 700
+T=transitivity_wu(mat);                                     % transitivity 701
+S=smallworldness(mat);                                      % smallworldness 702
+A=assortativity_wei(mat,0);                                 % assortivity 703
 
 % insert metrics in matrix
-metrics=[BC L GE C Ci EC Qi Q RC degree T S A]';
+metrics=[BC L GE C Ci EC Qi Q RC strength mean_strength T S A]';
 end
 
