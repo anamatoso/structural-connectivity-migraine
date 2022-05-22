b87779f58a33951c796c23d0a9102f09410a0055
Ana Matoso <anamatoso92@gmail.com>
2022-05-22 11:58:33 +0100

    put versions in metrics

--- matlab_scripts/calculate_metrics.m
+++ matlab_scripts/calculate_metrics.m
@@ -1,24 +1,65 @@
-function [metrics] = calculate_metrics(mat)
+function [metrics] = calculate_metrics(mat,version)
 % This function calculates the graph metrics given a connectivity matrix
 
-len_mat=1./mat;                 % conection-length matrix
-d_mat= distance_wei(len_mat);   % distance matrix
+if version==1
+    
+    len_mat=1./mat;                 % conection-length matrix
+    d_mat= distance_wei(len_mat);   % distance matrix
+    
+    %calculate metrics
+    BC=betweenness_wei(len_mat)';                               % betweenness centrality 1-116
+    [L,GE]=charpath(d_mat,0,0);                                     % characteristic path length and global efficiency 117,118
+    Ci=clustering_coef_wu(weight_conversion(mat, 'normalize'))';% local clustering coefficient 120-235
+    C=mean(Ci);                                                 % global clustering coefficient 119
+    EC=eigenvector_centrality_und(mat)';                        % eigenvector centrality 236-351
+    [Qi, Q]=modularity_und(mat);Qi=Qi';                         % modularity 352-467, 468
+    RC=rich_club_wu_norm(mat,115);                              % rich club coefficient 469-583
+    strength=strengths_und(mat);                                % node strength 584-699
+    mean_strength=mean(strength);                               % mean strength 700
+    T=transitivity_wu(mat);                                     % transitivity 701
+    S=smallworldness(mat);                                      % smallworldness 702
+    A=assortativity_wei(mat,0);                                 % assortivity 703
+    
+    % insert metrics in matrix
+    metrics=[BC L GE C Ci EC Qi Q RC strength mean_strength T S A]';
+    
+    
+elseif version==2
+    len_mat=1./mat;                 % conection-length matrix
+    d_mat= distance_wei(len_mat);   % distance matrix
+    
+    %calculate metrics
+    [L,GE]=charpath(d_mat,0,0);                                     % characteristic path length and global efficiency 1,2
+    Ci=clustering_coef_wu(weight_conversion(mat, 'normalize'))';    % local clustering coefficient 
+    C=mean(Ci);                                                     % global clustering coefficient 3    
+    [~, Q]=modularity_und(mat);                                          % modularity 4
+    strength=strengths_und(mat);                                    % node strength 5-120
+    mean_strength=mean(strength);                                   % mean strength 121
+    T=transitivity_wu(mat);                                         % transitivity 122
+    S=smallworldness(mat);                                          % smallworldness 123
+    A=assortativity_wei(mat,0);                                     % assortivity 124
+    
+    % insert metrics in matrix
+    metrics=[L GE C Q strength mean_strength T S A]';
 
-%calculate metrics
-BC=betweenness_wei(len_mat)';                               % betweenness centrality 1-116
-[L,GE]=charpath(d_mat,0,0);                                     % characteristic path length and global efficiency 117,118
-Ci=clustering_coef_wu(weight_conversion(mat, 'normalize'))';% local clustering coefficient 120-235
-C=mean(Ci);                                                 % global clustering coefficient 119
-EC=eigenvector_centrality_und(mat)';                        % eigenvector centrality 236-351
-[Qi, Q]=modularity_und(mat);Qi=Qi';                         % modularity 352-467, 468
-RC=rich_club_wu_norm(mat,115);                              % rich club coefficient 469-583
-strength=strengths_und(mat);                                % node strength 584-699
-mean_strength=mean(strength);                               % mean strength 700
-T=transitivity_wu(mat);                                     % transitivity 701
-S=smallworldness(mat);                                      % smallworldness 702
-A=assortativity_wei(mat,0);                                 % assortivity 703
 
-% insert metrics in matrix
-metrics=[BC L GE C Ci EC Qi Q RC strength mean_strength T S A]';
+elseif version==3
+    len_mat=1./mat;                 % conection-length matrix
+    d_mat= distance_wei(len_mat);   % distance matrix
+    
+    %calculate metrics
+    [L,GE]=charpath(d_mat,0,0);                                     % characteristic path length and global efficiency 1,2
+    Ci=clustering_coef_wu(weight_conversion(mat, 'normalize'));    % local clustering coefficient 
+    C=mean(Ci);                                                     % global clustering coefficient 3    
+    [~, Q]=modularity_und(mat);                                          % modularity 4
+    strength=strengths_und(mat);                                    % node strength 5-120
+    mean_strength=mean(strength);                                   % mean strength 121
+    T=transitivity_wu(mat);                                         % transitivity 122
+    S=smallworldness(mat);                                          % smallworldness 123
+    A=assortativity_wei(mat,0);                                     % assortivity 124
+    
+    % insert metrics in matrix
+    metrics=[L GE C Q mean_strength T S A]';
+end
 end
 
--- matlab_scripts/get_label_metrics.m
+++ matlab_scripts/get_label_metrics.m
@@ -1,30 +1,42 @@
-function [metrics_labels] = get_label_metrics()
+function [metrics_labels] = get_label_metrics(a)
 % This funtion prints the labels of each metric
-BC=strings(1,116);
-Ci=strings(1,116);
-EC=strings(1,116);
-Qi=strings(1,116);
-D=strings(1,116);
-RC=strings(1,115);
-node_labels=get_label_nodes("AAL116_labels.txt");
-for i=1:116
-    if i<=115
-        BC(i)="BC_"+node_labels(i);
-        Ci(i)="Ci_"+node_labels(i);
-        EC(i)="EC_"+node_labels(i);
-        Qi(i)="Qi_"+node_labels(i);
-        D(i)="D_"+node_labels(i);
-        RC(i)="RC_"+num2str(i);
-        
-    else
-        BC(i)="BC_"+node_labels(i);
-        Ci(i)="Ci_"+node_labels(i);
-        EC(i)="EC_"+node_labels(i);
-        Qi(i)="Qi_"+node_labels(i);
+if a==1
+    BC=strings(1,116);
+    Ci=strings(1,116);
+    EC=strings(1,116);
+    Qi=strings(1,116);
+    D=strings(1,116);
+    RC=strings(1,115);
+    node_labels=get_label_nodes("AAL116_labels.txt");
+    for i=1:116
+        if i<=115
+            BC(i)="BC_"+node_labels(i);
+            Ci(i)="Ci_"+node_labels(i);
+            EC(i)="EC_"+node_labels(i);
+            Qi(i)="Qi_"+node_labels(i);
+            D(i)="D_"+node_labels(i);
+            RC(i)="RC_"+num2str(i);
+            
+        else
+            BC(i)="BC_"+node_labels(i);
+            Ci(i)="Ci_"+node_labels(i);
+            EC(i)="EC_"+node_labels(i);
+            Qi(i)="Qi_"+node_labels(i);
+            D(i)="D_"+node_labels(i);
+        end
+    end
+    metrics_labels=[BC "L" "GE" "C" Ci EC Qi "Q" RC D "mean_D" "T" "S" "A"];
+    
+elseif a==2
+    D=strings(1,116);
+    node_labels=get_label_nodes("AAL116_labels.txt");
+    for i=1:116
         D(i)="D_"+node_labels(i);
     end
+    metrics_labels=["L" "GE" "C" "Q" D "mean_D" "T" "S" "A"];
+    
+elseif a==3
+    metrics_labels=["L" "GE" "C" "Q" "mean_D" "T" "S" "A"];
 end
-
-metrics_labels=[BC "L" "GE" "C" Ci EC Qi "Q" RC D "mean_D" "T" "S" "A"];
 end
 
