e987d3800fc299b1869b9adbd1d619f039743620
Ana Matoso <anamatoso92@gmail.com>
2022-05-26 15:01:21 +0100

    Update get_label_metrics.m

--- matlab_scripts/get_label_metrics.m
+++ matlab_scripts/get_label_metrics.m
@@ -1,15 +1,17 @@
 function [metrics_labels] = get_label_metrics(a,node_labels)
-% This funtion prints the labels of each metric
+% This funtion prints the labels of each metric given a choice of metrics and the label of each node
+
+n_nodes=length(node_labels);
 if a==1
-    BC=strings(1,116);
-    Ci=strings(1,116);
-    EC=strings(1,116);
-    Qi=strings(1,116);
-    D=strings(1,116);
-    RC=strings(1,115);
+    BC=strings(1,n_nodes);
+    Ci=strings(1,n_nodes);
+    EC=strings(1,n_nodes);
+    Qi=strings(1,n_nodes);
+    D=strings(1,n_nodes);
+    RC=strings(1,n_nodes);
     
-    for i=1:116
-        if i<=115
+    for i=1:n_nodes
+        if i<=n_nodes-1
             BC(i)="BC_"+node_labels(i);
             Ci(i)="Ci_"+node_labels(i);
             EC(i)="EC_"+node_labels(i);
@@ -28,9 +30,8 @@ if a==1
     metrics_labels=[BC "L" "GE" "C" Ci EC Qi "Q" RC D "mean_D" "T" "S" "A"];
     
 elseif a==2
-    D=strings(1,116);
-    node_labels=get_label_nodes("AAL116_labels.txt");
-    for i=1:116
+    D=strings(1,n_nodes);
+    for i=1:n_nodes
         D(i)="D_"+node_labels(i);
     end
     metrics_labels=["L" "GE" "C" "Q" D "mean_D" "T" "S" "A"];
