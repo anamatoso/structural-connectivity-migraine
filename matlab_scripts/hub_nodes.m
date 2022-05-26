f379f1b6dc435c1de1fc2c3b6fc354b754d7e8e6
Ana Matoso <anamatoso92@gmail.com>
2022-05-26 16:33:09 +0100

    Update hub_nodes.m

--- matlab_scripts/hub_nodes.m
+++ matlab_scripts/hub_nodes.m
@@ -1,9 +1,9 @@
-function [I] = hub_nodes(conmat)
+function [I,values] = hub_nodes(conmat)
 len_mat=1./conmat;                 % conection-length matrix
 BC=betweenness_wei(len_mat)'; 
 
 P = prctile(BC,80);
 I = find(BC >= P);
-
+values=BC(I);
 end
 
