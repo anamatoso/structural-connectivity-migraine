69d86e71f799edb6953ca87c24824929251d7dd8
Ana Matoso <anamatoso92@gmail.com>
2022-05-26 14:23:51 +0100

    Update load_data_mrtrix.m

--- matlab_scripts/load_data_mrtrix.m
+++ matlab_scripts/load_data_mrtrix.m
@@ -5,7 +5,11 @@ function [connectomes] = load_data_mrtrix(folder,pattern)
 F = folder;
 filePattern = fullfile(F, pattern); % Change to whatever pattern you need.
 theFiles = dir(filePattern);
-connectomes=zeros(116,116,length(theFiles));
+baseFileName = theFiles(1).name;
+fullFileName = fullfile(theFiles(1).folder, baseFileName);
+c=importdata(fullFileName);
+connectomes=zeros(length(c),length(c),length(theFiles));
+
 for k = 1 : length(theFiles)
     baseFileName = theFiles(k).name;
     fullFileName = fullfile(theFiles(k).folder, baseFileName);
