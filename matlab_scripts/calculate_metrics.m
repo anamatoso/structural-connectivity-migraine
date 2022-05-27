baf16dc41feabf4bc3075708ecca438251309d6b
Ana Matoso <anamatoso92@gmail.com>
2022-05-27 11:48:27 +0100

    Update calculate_metrics.m

--- matlab_scripts/calculate_metrics.m
+++ matlab_scripts/calculate_metrics.m
@@ -13,7 +13,7 @@ if version==1
     Ci=clustering_coef_wu(weight_conversion(mat, 'normalize'))';% local clustering coefficient 120-235
     C=mean(Ci);                                                 % global clustering coefficient 119
     EC=eigenvector_centrality_und(mat)';                        % eigenvector centrality 236-351
-    [Qi, Q]=modularity_und(mat);Qi=Qi';                         % modularity 352-467, 468
+    [Qi, Q]=modularity_und(mat);                                % modularity 352-467, 468
     RC=rich_club_wu_norm(mat,n_nodes-1);                        % rich club coefficient 469-583
     strength=strengths_und(mat);                                % node strength 584-699
     mean_strength=mean(strength);                               % mean strength 700
@@ -22,7 +22,7 @@ if version==1
     A=assortativity_wei(mat,0);                                 % assortivity 703
     
     % insert metrics in matrix
-    metrics=[BC L GE C Ci EC Qi Q RC strength mean_strength T S A]';
+    metrics=[BC L GE C Ci EC Q RC strength mean_strength T S A]';
     
     
 elseif version==2
