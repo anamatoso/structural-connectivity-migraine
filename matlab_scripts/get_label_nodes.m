058319ac46c7d4a8e3734bddc6244b73545dac1b
Ana Matoso <anamatoso92@gmail.com>
2022-05-28 11:45:50 +0100

    Update get_label_nodes.m

--- matlab_scripts/get_label_nodes.m
+++ matlab_scripts/get_label_nodes.m
@@ -1,5 +1,5 @@
 function [labels] = get_label_nodes(file)
-% This funtion gets the labels of each metric
+% This funtion gets the labels of each metric from a txt file
 cell_labels=importdata(file);
 n_nodes=length(cell_labels);
 labels=strings(1,n_nodes);
