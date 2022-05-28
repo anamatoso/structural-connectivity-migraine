094a2144d8126ef6abc5e6d60dcca4f5f47aaae3
Ana Matoso <anamatoso92@gmail.com>
2022-05-28 16:21:56 +0100

    Update anova_compare.m

--- matlab_scripts/anova_compare.m
+++ matlab_scripts/anova_compare.m
@@ -1,14 +1,18 @@
-function [ANOVA_results] = anova_compare(metrics,metrics_labels,varargin)
+function [ANOVA_results] = anova_compare(metrics,metrics_labels,version_metrics,varargin)
 % Given the metrics and the respective labels, this function calculates which metrics are significantly different. If varargin is set to "True" then it shows all metrics (instead of only the significant ones).
 
-n_metrics=length(metrics{1});
+n_metrics=length(metrics_labels);
 compare_anova=zeros(1,5);
 
 if ~isempty(varargin)
     if varargin{1}=="True"
         threshold=inf;
     else
-        threshold=0.05/n_metrics;
+        if version_metrics==3
+            threshold=0.05;
+        else
+            threshold=0.05/n_metrics;
+        end
     end
 end
 
@@ -41,6 +45,7 @@ for m=1:n_metrics
         end
     end
 end
+
 compare_anova=compare_anova(2:end,:);
 table=array2table(compare_anova, "VariableNames", ["Metric index","Group 1", "Group 2", "P-value (corrected)", "Difference"]);
 metrics_names=metrics_labels(compare_anova(:,1))';
