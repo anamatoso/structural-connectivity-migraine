635029ad308e44217e9dba047f70cb8044613c46
Ana Matoso <anamatoso92@gmail.com>
2022-05-31 23:53:28 +0100

    Update plot_boxplots.m

--- matlab_scripts/plot_boxplots.m
+++ matlab_scripts/plot_boxplots.m
@@ -1,8 +1,16 @@
-function [] = plot_boxplots(metrics,idx,metrics_labels)
+function [] = plot_boxplots(metrics,idx,metrics_labels,version_metrics)
 n_metrics=length(metrics_labels);
+
+if version_metrics==3
+    correction=1;
+else
+    correction=n_metrics;
+end
+
+figure;
 for i=1:length(idx)
     index=idx(i);
-    figure;
+    subplot(1,2,i)
     
     hc_mid=metrics{1}(index,:);
     mig_inter=metrics{2}(index,:);
@@ -10,14 +18,14 @@ for i=1:length(idx)
     x = [hc_mid mig_inter];
     g = [zeros(1,length(hc_mid)),ones(1,length(mig_inter))];
     
-    boxplot(x,g,'Labels',{'HC','M'})
-    title(metrics_labels(index),'interpreter', 'none')
-    [h,p]=ttest2(hc_mid,mig_inter,'Alpha',0.05/n_metrics);
+    boxplot(x,g,'Labels',{'HC','M'},'width',0.8)
+    title(metrics_labels(index),'interpreter', 'none','FontSize',20,'FontWeight','normal','FontName','Arial')
+    [h,p]=ttest2(hc_mid,mig_inter,'Alpha',0.05/correction);
     if h==1
         hold on
-        text(2.1,1.01*quantile(mig_inter,0.75), '*','FontSize',15,'Color','black');
+        %text(2.1,1.01*quantile(mig_inter,0.75), '*','FontSize',15,'Color','black');
     end
-    text(2.2,max(max(mig_inter),max(hc_mid)), "p="+num2str(p*n_metrics),'FontSize',10,'Color','black');
+    text(2.2,1.01*max(max(mig_inter),max(hc_mid)), "p="+num2str(p*correction,2),'FontSize',15,'Color','black');
 
     hold off
 end
