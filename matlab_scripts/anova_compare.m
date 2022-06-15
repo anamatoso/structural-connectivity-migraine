4ebfc80bad6b88e7285dce9e1f646dd58cfdbc62
Ana Matoso <anamatoso92@gmail.com>
2022-06-15 18:19:14 +0100

    Update anova_compare.m

--- matlab_scripts/anova_compare.m
+++ matlab_scripts/anova_compare.m
@@ -22,48 +22,30 @@ end
 for m=1:n_metrics
     hc_mid=metrics{1}(m,:);
     mig_inter=metrics{2}(m,:);
-    %hc_pre=metrics{3}(m,:);
-    %mig_ict=metrics{4}(m,:);
+    hc_pre=metrics{3}(m,:);
+    mig_ict=metrics{4}(m,:);
     
     x = [hc_mid mig_inter];% hc_pre mig_ict];
     g = [zeros(1,length(hc_mid)),ones(1,length(mig_inter))];%,2.*ones(1,length(hc_pre)),3.*ones(1,length(mig_ict))];
     
     if any(isnan(x))
-        %compare_anova=[compare_anova;m 0 0 +inf 0];
+        compare_anova=[compare_anova;m 0 0 correction 0];
         continue
     end
     
     [~, ~, stats] = anova1(x,g,'off');
-    c=multcompare(stats,'display','off','alpha',0.05/correction);
+    c=multcompare(stats,'display','off','alpha',0.05/correction,'ctype','bonferroni');
     
-%     k=kstest(hc_mid) && kstest(mig_inter);
-%     if k==1
-%         [p,~] = ranksum(hc_mid,mig_inter,'alpha',0.05/correction);    
-%     else
-%         [~,p] = ttest2(hc_mid,mig_inter,'alpha',0.05/correction);
-%     end
-%     
-%     
-%     if p<threshold
-%         switch mean(hc_mid)-mean(mig_inter)>0 % difference is positive?
-%             case 1
-%                 compare_anova=[compare_anova;m 1 2 p*correction 1];
-%             case 0
-%                 compare_anova=[compare_anova;m 1 2 p*correction -1];
-%         end
-%     end
-    
-    
-        for idx_p=1:length(c(:,end))
-            if c(idx_p,end)<threshold
-                switch c(idx_p,4)>0 % difference is positive?
-                    case 1
-                        compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end)*correction 1];
-                    case 0
-                        compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end)*correction -1];
-                end
+    for idx_p=1:length(c(:,end))
+        if c(idx_p,end)<threshold
+            switch c(idx_p,4)>0 % difference is positive?
+                case 1
+                    compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end)*correction 1];
+                case 0
+                    compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end)*correction -1];
             end
         end
+    end
     
 end
 
