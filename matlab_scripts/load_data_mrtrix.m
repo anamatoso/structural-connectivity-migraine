bc1d06609c42559ccb6d6b78b892c098388a1367
Ana Matoso <anamatoso92@gmail.com>
2022-06-21 16:37:13 +0100

    Update load_data_mrtrix.m

--- matlab_scripts/load_data_mrtrix.m
+++ matlab_scripts/load_data_mrtrix.m
@@ -1,4 +1,4 @@
-function [connectomes] = load_data_mrtrix(folder,pattern)
+function [connectomes] = load_data_mrtrix(folder,pattern,folder_roi_sizes)
 % This function load the data from a given folder which have a given
 % pattern
 
@@ -12,8 +12,9 @@ connectomes=zeros(length(c),length(c),length(theFiles));
 
 for k = 1 : length(theFiles)
     baseFileName = theFiles(k).name;
+    roi_file=strcat(folder_roi_sizes,"/",baseFileName(1:length(baseFileName)-34),"_roi_size_intersect.txt"); %17
     fullFileName = fullfile(theFiles(k).folder, baseFileName);
-    c=importdata(fullFileName);
+    c=normalize_roisize_mrtrix(fullFileName,roi_file);
     connectomes(:,:,k)=c;
 end
 end
