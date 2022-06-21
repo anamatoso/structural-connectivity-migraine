ed1f3ccaf32fd07ff054af27e677a2754a0d9f0f
Ana Matoso <anamatoso92@gmail.com>
2022-06-21 16:37:32 +0100

    Update anova_compare.m

--- matlab_scripts/anova_compare.m
+++ matlab_scripts/anova_compare.m
@@ -28,8 +28,8 @@ for m=1:n_metrics
     x = [hc_mid mig_inter];% hc_pre mig_ict];
     g = [zeros(1,length(hc_mid)),ones(1,length(mig_inter))];%,2.*ones(1,length(hc_pre)),3.*ones(1,length(mig_ict))];
     
-    if any(isnan(x))
-        compare_anova=[compare_anova;m 0 0 correction 0];
+    if any(isnan(x)) || all(x==0)
+        compare_anova=[compare_anova;m 1 2 correction 0];
         continue
     end
     
@@ -40,9 +40,9 @@ for m=1:n_metrics
         if c(idx_p,end)<threshold
             switch c(idx_p,4)>0 % difference is positive?
                 case 1
-                    compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end)*correction 1];
+                    compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end) 1];
                 case 0
-                    compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end)*correction -1];
+                    compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end) -1];
             end
         end
     end
@@ -50,7 +50,7 @@ for m=1:n_metrics
 end
 
 compare_anova=compare_anova(2:end,:);
-table=array2table(compare_anova, "VariableNames", ["Metric index","Group 1", "Group 2", "P-value (corrected)", "Difference"]);
+table=array2table(compare_anova, "VariableNames", ["Metric index","Group 1", "Group 2", "P-value", "Difference"]);
 metrics_names=metrics_labels(compare_anova(:,1))';
 t_names=array2table(metrics_names, "VariableNames", ["Metric Name"]);
 ANOVA_results = [t_names table];
