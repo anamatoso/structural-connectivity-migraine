4d4d0b29fb5019d153a648b4faf3d8782bae09e5
Ana Matoso <anamatoso92@gmail.com>
2022-06-21 16:37:39 +0100

    Update anova2matrix.m

--- matlab_scripts/anova2matrix.m
+++ matlab_scripts/anova2matrix.m
@@ -1,10 +1,28 @@
-function [matrix] = anova2matrix(anovacompare)
+function [matrix] = anova2matrix(anovacompare,sign)
 
-table=table2array(anovacompare(:,3:8));
+table=table2array(anovacompare(:,3:9));
 
-for i=1:length(table)
-    matrix(table(i,1),table(i,2))=table(i,6)*table(i,5);
-    matrix(table(i,2),table(i,1))=table(i,6)*table(i,5);
+if sign=="pos"
+    for i=1:length(table)
+        if table(i,7)>0
+            matrix(table(i,1),table(i,2))=table(i,6);
+            matrix(table(i,2),table(i,1))=table(i,6);
+        else
+            matrix(table(i,1),table(i,2))=0;
+            matrix(table(i,2),table(i,1))=0;
+        end
+    end
+    
+else
+    for i=1:length(table)
+        if table(i,7)<0
+            matrix(table(i,1),table(i,2))=-table(i,6);
+            matrix(table(i,2),table(i,1))=-table(i,6);
+        else
+            matrix(table(i,1),table(i,2))=0;
+            matrix(table(i,2),table(i,1))=0;
+        end
+    end
 end
 end
 
