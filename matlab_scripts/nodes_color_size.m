75286eeb548704fefbb48253f2ce45de158fe070
Ana Matoso <anamatoso92@gmail.com>
2022-06-21 16:37:03 +0100

    Update nodes_color_size.m

--- matlab_scripts/nodes_color_size.m
+++ matlab_scripts/nodes_color_size.m
@@ -1,18 +1,18 @@
 function [table] = nodes_color_size(pvalues,difference,threshold,atlas_labels)
 
-% size: small (1) for insignifican nodes, and big (2) for significant nodes
-% color: 1 for insignificant, 3 for positive, 2 for negative
+% size: small (1) for insignifican nodes, and big (3) for significant nodes
+% color: 1 for insignificant, 2 for positive, 0 for negative
 n_nodes=length(atlas_labels);
-table=zeros(n_nodes,2);
+table=zeros(n_nodes,2); %color | size
 
 for i=1:n_nodes
     
     if pvalues(i)<threshold %"significant" diference
-        table(i,2)=2;
+        table(i,2)=3;
         if difference(i)==-1
-            table(i,1)=3;
-        else
             table(i,1)=2;
+        else
+            table(i,1)=0;
         end
     else %not "significant" diference
         table(i,2)=1;
