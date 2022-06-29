5728f095f45c6a0fa4869c6f29c435df99e432a2
Ana Matoso <anamatoso92@gmail.com>
2022-06-29 10:35:42 +0100

    Update normalize_roisize_mrtrix.m

--- matlab_scripts/normalize_roisize_mrtrix.m
+++ matlab_scripts/normalize_roisize_mrtrix.m
@@ -6,13 +6,14 @@ function [connectome] = normalize_roisize_mrtrix(filename,roi_size)
 connectome=importdata(filename);
 
 size_roi=importdata(roi_size);%.*8; %get size of rois in voxels and multiply by volume per voxel
-sum_volume=sum(size_roi);
-%Normalize MRTrix connectome with sizes of ROIs
+mean_volume=mean(size_roi);
+
+%Normalize MRTrix connectome with mean size of ROIs
 n_nodes=length(connectome);
 for n=1:n_nodes
     for m=1:n_nodes
         if n~=m
-            connectome(n,m)=connectome(n,m)*sum_volume;
+            connectome(n,m)=connectome(n,m)*mean_volume;
         end
     end
 end
