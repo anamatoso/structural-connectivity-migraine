a526c648a9ec50b1474990c4cb60037bb72dd982
Ana Matoso <anamatoso92@gmail.com>
2022-06-29 10:35:48 +0100

    Update normalize_roisize_fsl.m

--- matlab_scripts/normalize_roisize_fsl.m
+++ matlab_scripts/normalize_roisize_fsl.m
@@ -5,7 +5,6 @@ function [connectome] = normalize_roisize_fsl(filename,roi_size)
 % count is given.
 
 connectome=importdata(filename);
-
 size_roi=importdata(roi_size);%.*8; %get size of rois in voxels and multiply by volume per voxel
 
 %Normalize FSL connectome with sizes of ROIs
