e01a605bbd7533f842ab9da17aa0e09e7a41ca20
Ana Matoso <anamatoso92@gmail.com>
2022-06-30 11:57:40 +0100

    alter to use with multiple comparisons

--- matlab_scripts/plot_boxplots.m
+++ matlab_scripts/plot_boxplots.m
@@ -1,32 +1,38 @@
-function [] = plot_boxplots(metrics,idx,metrics_labels,version_metrics,n_nodes)
-n_metrics=length(metrics_labels);
+function [] = plot_boxplots(metrics,idx_metrics,idx_groups,metrics_labels,patient_labels)
 
-if version_metrics==3
-    correction=1;
-else
-    correction=n_nodes;
-end
+% if version_metrics==2
+%     correction=1;
+% else
+%     correction=n_nodes;
+% end
 
-figure('color','w');
-for i=1:length(idx)
-    index=idx(i);
-    subplot(1,length(idx),i)
+labels=patient_labels(idx_groups);
+
+nmetrics=length(idx_metrics);
+for i=1:nmetrics
+    figure('color','w');
+    metric=idx_metrics(i);
     
-    hc_mid=metrics{1}(index,:);
-    mig_inter=metrics{2}(index,:);
-    
-    x = [hc_mid mig_inter];
-    g = [zeros(1,length(hc_mid)),ones(1,length(mig_inter))];
-    
-    boxplot(x,g,'Labels',{'HC','M'})
-    title(metrics_labels(index),'interpreter', 'none','FontSize',15,'FontWeight','normal','FontName','Arial')
-    [h,p]=ttest2(hc_mid,mig_inter,'Alpha',0.05/correction);
-    if h==1
-        hold on
-        text(2.1,1.01*quantile(mig_inter,0.75), '*','FontSize',15,'Color','black');
+    x=[];
+    group=[];
+    for g=1:length(idx_groups)
+        x=[x metrics{idx_groups(g)}(metric,:)];
+        group=[group (g-1)*ones(size(metrics{idx_groups(g)}(metric,:)))];
     end
-    %text(2.2,1.01*max(max(mig_inter),max(hc_mid)), "p="+num2str(p*correction,2),'FontSize',15,'Color','black');
-    hold off
+    
+    
+    boxplot(x,group,'Labels',labels)
+    title(metrics_labels(metric),'interpreter', 'none','FontSize',15,'FontWeight','normal','FontName','Arial')
+
+    %      [h,p]=ttest2(g1,g2,'Alpha',0.05/correction);
+%      if h==1
+%          hold on
+%          text(2.1,1.01*quantile(g2,0.75), '*','FontSize',15,'Color','black');
+%      end
+     %text(2.2,1.01*max(max(mig_inter),max(hc_mid)), "p="+num2str(p*correction,2),'FontSize',15,'Color','black');
+%     hold off
+    %ax = gca;
+    %ax.YAxis.Scale ="log";
 end
 
 end
--- matlab_scripts/plot_boxplots_conn.m
+++ matlab_scripts/plot_boxplots_conn.m
@@ -1,28 +1,30 @@
-function [] = plot_boxplots_conn(connectomes,idx,node_labels)
+function [] = plot_boxplots_conn(connectomes,idx_metrics,idx_groups,patient_labels,node_labels)
 
 n_nodes=length(node_labels);
 n_comparisons=(n_nodes*n_nodes-n_nodes)/2;
+labels=patient_labels(idx_groups);
 
-for i=1:length(idx)
-    index1=idx(i,1);
-    index2=idx(i,2);
+for i=1:length(idx_metrics)
+    index1=idx_metrics(i,1);
+    index2=idx_metrics(i,2);
     figure('color','w');
-    
-    hc_mid=squeeze(connectomes{1}(index1,index2,:))';
-    mig_inter=squeeze(connectomes{2}(index1,index2,:))';
-    
-    x = [hc_mid mig_inter];
-    g = [zeros(1,length(hc_mid)),ones(1,length(mig_inter))];
-    
-    boxplot(x,g,'Labels',{'HC','M'})
-    title(node_labels(index1)+"-"+node_labels(index2),'interpreter', 'none')
-    [h,p]=ttest2(hc_mid,mig_inter,'Alpha',0.05/n_comparisons);
-    if h==1
-        hold on
-        text(2.1,1.01*quantile(mig_inter,0.75), '*','FontSize',15,'Color','black');
+
+    x=[];
+    group=[];
+    for g=1:length(idx_groups)
+        x=[x squeeze(connectomes{g}(index1,index2,:))'];
+        group=[group (g-1)*ones(size(squeeze(connectomes{g}(index1,index2,:))'))];
     end
-    text(2.2,1.01*max(max(mig_inter),max(hc_mid)), "p="+num2str(p*n_comparisons),'FontSize',10,'Color','black');
-    hold off
+    
+    boxplot(x,group,'Labels',labels)
+    title(node_labels(index1)+"-"+node_labels(index2),'interpreter', 'none')
+%     [h,p]=ttest2(hc_mid,mig_inter,'Alpha',0.05/n_comparisons);
+%     if h==1
+%         hold on
+%         text(2.1,1.01*quantile(mig_inter,0.75), '*','FontSize',15,'Color','black');
+%     end
+%     text(2.2,1.01*max(max(mig_inter),max(hc_mid)), "p="+num2str(p*n_comparisons),'FontSize',10,'Color','black');
+%     hold off
 end
 
 end
