a83beaeb51c4aee6c84a761a4709af1503c384f7
Ana Matoso <anamatoso92@gmail.com>
2022-06-30 12:04:45 +0100

    Update FSL_01_bedpostx.sh

--- shell_scripts/FSL_01_bedpostx.sh
+++ shell_scripts/FSL_01_bedpostx.sh
@@ -5,9 +5,7 @@ MAINDIR=$(pwd) #data
 display_usage() {
 	echo "$(basename $0) [Subject and type of session]"
 	# echo "This script uses FSL to analyze diffusion data. It requires X arguments: 
-	# 	1) Subject DWI Directory (string)- has to have data.nii.gz (...b0b1b2000.nii.gz), nodif_brain_mask.nii.gz, bvecs (...b0b1000b2000), bvals
-	#	example: "sub-control019"
-	#	2) Type of session. Example: ses-midcycle
+	# 	1) Subject Directory, example: "sub-control019_ses-midcycle
 	}
 
 	if [ $# -le 0 ] # if there are 0 argument or less
@@ -24,14 +22,15 @@ SUBDIR=$1 #example name: sub-control019_ses-midcycle
 
 # create bedpost directory inside subject and copy files to there
 cd $SUBDIR
+
+# CHECK IF NEXT ACTIONS ARE ALREADY PERFORMED FOR THE SUBJECTS
 mkdir -p bedpostdata
 cp "018_${SUBDIR}_b0b1000b2000.nii.gz" ./bedpostdata/data.nii.gz
 cp "017_${SUBDIR}_clean_mask.nii.gz" ./bedpostdata/nodif_brain_mask.nii.gz
 cp "018_${SUBDIR}_b0b1000b2000_bvals.bval" ./bedpostdata/bvals
 cp "018_${SUBDIR}_b0b1000b2000_bvecs.bvec" ./bedpostdata/bvecs
 
-
-# check if its all good. It should be.
+# check if its all good. It should be
 # bedpostx_datacheck bedpostdata
 
 ########################### STEP 1 ###################################
