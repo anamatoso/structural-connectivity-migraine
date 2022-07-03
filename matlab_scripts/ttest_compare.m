dcd41a5448647911c680e60b4c19e17e3aeaa824
Ana Matoso <anamatoso92@gmail.com>
2022-07-03 18:24:19 +0100

    Update ttest_compare.m

--- matlab_scripts/ttest_compare.m
+++ matlab_scripts/ttest_compare.m
@@ -1,6 +1,6 @@
 function [ttest_results] = ttest_compare(metrics,metrics_labels,version_metrics,n_nodes,comparisons)
 % Given the metrics and the respective labels, this function calculates
-% which metrics are significantly different. 
+% which metrics are significantly different.
 
 n_metrics=length(metrics_labels);
 compare_ttest=zeros(1,6);
@@ -16,7 +16,7 @@ for c=1:ncomp
     g2=comparisons(c,2);
     for m=1:n_metrics
         data1=metrics{g1}(m,:);
-        data2=metrics{g2}(m,:);  
+        data2=metrics{g2}(m,:);
         
         x = [data1 data2];
         g = [zeros(1,length(data1)),ones(1,length(data2))];
@@ -32,7 +32,7 @@ for c=1:ncomp
                 compare_ttest=[compare_ttest;m g1 g2 p p*correction 1];
             case 0
                 compare_ttest=[compare_ttest;m g1 g2 p p*correction -1];
-        end  
+        end
     end
 end
 
@@ -47,18 +47,18 @@ ttest_results = [t_names table];
 
 ttest_results = [t_names table];
 if version_metrics==3
-% Calculate FDR for nodal metrics
-pvals=reshape(table2array(ttest_results(:,5)),116,[]);
-[~,nmetrics]=size(pvals);
-fdr=zeros(size(pvals));
-for i=1:nmetrics
-    [~, q] = mafdr(pvals(:,i));
-    fdr(:,i)=q;
-end
-
-fdr=reshape(fdr,[],1);
-fdr_table=array2table(fdr, "VariableNames", ["q-value"]);
-ttest_results = [t_names table fdr_table];
+    % Calculate FDR for nodal metrics
+    pvals=reshape(table2array(ttest_results(:,5)),116,[]);
+    [~,nmetrics]=size(pvals);
+    fdr=zeros(size(pvals));
+    for i=1:nmetrics
+        [~, q] = mafdr(pvals(:,i));
+        fdr(:,i)=q;
+    end
+    
+    fdr=reshape(fdr,[],1);
+    fdr_table=array2table(fdr, "VariableNames", ["q-value"]);
+    ttest_results = [t_names table fdr_table];
 end
 
 end
