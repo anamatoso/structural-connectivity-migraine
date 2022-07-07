3cec10dfc46d1f6725d4ecaf8e23a74e294622d5
Ana Matoso <anamatoso92@gmail.com>
2022-07-07 10:12:50 +0100

    Update nodes_color_size.m

--- matlab_scripts/nodes_color_size.m
+++ matlab_scripts/nodes_color_size.m
@@ -9,7 +9,7 @@ for i=1:n_nodes
     
     if pvalues(i)<=threshold %"significant" diference
         table(i,2)=3;
-        if difference(i)==-1
+        if difference(i)==1
             table(i,1)=2;
         else
             table(i,1)=0;
