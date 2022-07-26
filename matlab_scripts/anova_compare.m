9e52465f2f718c740148af411a7bb510e1dd34e6
Ana Matoso <anamatoso92@gmail.com>
2022-07-26 15:46:51 +0100

    Update anova_compare.m

--- matlab_scripts/anova_compare.m
+++ matlab_scripts/anova_compare.m
@@ -1,52 +1,50 @@
-function [ANOVA_results] = anova_compare(metrics,metrics_labels,version_metrics,n_nodes,varargin)
+function [ANOVA_results] = anova_compare(metrics,metrics_labels,version_metrics,n_nodes)
 % Given the metrics and the respective labels, this function calculates
 % which metrics are significantly different. If varargin is set to "True"
 % then it shows all metrics (instead of only the significant ones).
 n_metrics=length(metrics_labels);
 compare_anova=zeros(1,5);
 
-if version_metrics==3
+if version_metrics==2
     correction=1;
 else
     correction=n_nodes;%n_metrics;
 end
 
-if ~isempty(varargin)
-    if varargin{1}=="True"
-        threshold=inf;
-    else
-        threshold=0.05/correction;
-    end
-end
-
 for m=1:n_metrics
-    hc_mid=metrics{1}(m,:);
-    mig_inter=metrics{2}(m,:);
-    hc_pre=metrics{3}(m,:);
-    mig_ict=metrics{4}(m,:);
-    
-    x = [hc_mid mig_inter];% hc_pre mig_ict];
-    g = [zeros(1,length(hc_mid)),ones(1,length(mig_inter))];%,2.*ones(1,length(hc_pre)),3.*ones(1,length(mig_ict))];
-    
-    if any(isnan(x)) || all(x==0)
+    data1=metrics{1}(m,:);
+    data2=metrics{2}(m,:);
+    data3=metrics{3}(m,:);
+    data4=metrics{4}(m,:);
+
+    x = [data1 data2 data3 data4];
+    g = [ones(1,length(data1)),2*ones(1,length(data2)),...
+        3*ones(1,length(data2)),4*ones(1,length(data2))];
+
+    if any(isnan(x))
         compare_anova=[compare_anova;m 1 2 correction 0];
+        continue
+    elseif all(x==0)
+        compare_ttest=[compare_ttest;m g1 g2 0 0 0];
         continue
     end
     
-    [~, ~, stats] = anova1(x,g,'off');
-    c=multcompare(stats,'display','off','alpha',0.05/correction,'ctype','bonferroni');
+    allnorm=(1-kstest(data1))*(1-kstest(data2))*(1-kstest(data3))*(1-kstest(data4));
     
+    if kstest(data1) && 
+    [~, ~, stats] = anova1(x,g,'off');
+    
+    
+    c=multcompare(stats,'display','off','alpha',0.05/correction,'ctype','bonferroni');
+
     for idx_p=1:length(c(:,end))
-        if c(idx_p,end)<threshold
-            switch c(idx_p,4)>0 % difference is positive?
-                case 1
-                    compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end) 1];
-                case 0
-                    compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end) -1];
-            end
+        switch c(idx_p,4)>0 % difference is positive?
+            case 1
+                compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end) 1];
+            case 0
+                compare_anova=[compare_anova;m c(idx_p,1) c(idx_p,2) c(idx_p,end) -1];
         end
     end
-    
 end
 
 compare_anova=compare_anova(2:end,:);
