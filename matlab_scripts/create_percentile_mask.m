97c95be70c86af93fb3934d9ebad2ea258884f84
Ana Matoso <anamatoso92@gmail.com>
2022-07-26 15:46:24 +0100

    Update create_percentile_mask.m

--- matlab_scripts/create_percentile_mask.m
+++ matlab_scripts/create_percentile_mask.m
@@ -1,11 +1,9 @@
 function [significance_mask] = create_percentile_mask(group_matrix,threshold)
 % a is matrix of size 116x116xn_people. Comes from load data.
 % significance_mask is 1 is connection is significant and 0 otherwise
-% mode=1 is numer of fibers, mode=2 is percentil
 
 [n_nodes,~,n_people]=size(group_matrix);
-significance_mask=zeros(n_nodes,n_nodes);
-
+significance_mask=zeros(size(group_matrix));
 
 for p=1:n_people
     matrix=squeeze(group_matrix(:,:,p));
@@ -13,8 +11,8 @@ for p=1:n_people
     for i=1:n_nodes-1
         for j=i:n_nodes
             if matrix(i,j)>percentile
-                significance_mask(i,j)=1;
-                significance_mask(j,i)=1;
+                significance_mask(i,j,p)=1;
+                significance_mask(j,i,p)=1;
             end
         end
     end
