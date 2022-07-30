bc3948e7961cbeb33db954db5b2b7627c2611695
Ana Matoso <anamatoso92@gmail.com>
2022-07-30 21:08:33 +0100

    Update random_mat.m

--- matlab_scripts/random_mat.m
+++ matlab_scripts/random_mat.m
@@ -1,14 +1,14 @@
 function [randmat] = random_mat(mat,nperm)
-
+nnodes=length(mat);
 randmat=mat;
 for i=1:nperm
     %nodes randomly chosen
-    n11=randi(116);n12=randi(116);
-    n21=randi(116);n22=randi(116);
+    n11=randi(nnodes);n12=randi(nnodes);
+    n21=randi(nnodes);n22=randi(nnodes);
     
     while n11==n12 || n21==n22
-        n11=randi(116);n12=randi(116);
-        n21=randi(116);n22=randi(116);
+        n11=randi(nnodes);n12=randi(nnodes);
+        n21=randi(nnodes);n22=randi(nnodes);
     end
     val1=randmat(n11,n12);
     val2=randmat(n21,n22);
