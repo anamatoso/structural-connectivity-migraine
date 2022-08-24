19f87e79e0cd804b5fd39409eb3afd9ebdfd1f1d
Ana Matoso <anamatoso92@gmail.com>
2022-08-24 15:35:37 +0100

    change normalize_roi_size

--- matlab_scripts/load_data.m
+++ matlab_scripts/load_data.m
@@ -16,22 +16,17 @@ connectomes=zeros(length(c),length(c),length(theFiles));
 
 for k = 1 : length(theFiles)
     baseFileName = theFiles(k).name;
+    fullFileName = fullfile(theFiles(k).folder, baseFileName);
+
     if algorithm=="fsl"
         roi_file=strcat(folder_roi_sizes,"/",baseFileName(1:length(baseFileName)-23),"_roi_size.txt"); %17, or 23 for omat3
-        fullFileName = fullfile(theFiles(k).folder, baseFileName);
-        c=normalize_roisize_fsl(fullFileName,roi_file,threshold);
-        %c=(c+c')/2;
-        
     elseif algorithm=="mrtrix"
         roi_file=strcat(folder_roi_sizes,"/",baseFileName(1:length(baseFileName)-34),"_roi_size_intersect.txt"); %34 aal, 32 desikan
-        fullFileName = fullfile(theFiles(k).folder, baseFileName);
-        %disp(roi_file)
-        c=normalize_roisize_mrtrix(fullFileName,roi_file,threshold);
-    
     else
         disp ('Error: Algorithm not known. Please choose either fsl or mrtrix.')
         return
     end
+    c=normalize_roisize(fullFileName,roi_file,threshold,algorithm);
     %c=c/sum(sum(c));
     connectomes(:,:,k)=c;
 end
