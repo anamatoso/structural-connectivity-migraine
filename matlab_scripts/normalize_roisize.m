0b0f613b4af349482d89a108005912e3dbacecf6
Ana Matoso <anamatoso92@gmail.com>
2022-08-24 15:35:50 +0100

    Create normalize_roisize.m

---
+++ matlab_scripts/normalize_roisize.m
@@ -0,0 +1,42 @@
+function [connectome] = normalize_roisize(filename,roi_size,threshold,algorithm)
+% This function normalizes the connectivity matrix by the sum of volumes of
+% all ROIs. As input, besides the connectivity matrix the list of ROI sizes
+% in voxel count is given.
+
+connectome=importdata(filename);
+
+size_roi=importdata(roi_size);%.*8; %get size of rois in voxels and multiply by volume per voxel
+mean_volume=mean(size_roi);
+sum_volume=sum(size_roi);
+
+
+%Normalize connectome
+n_nodes=length(connectome);
+for n=1:n_nodes
+    for m=1:n_nodes
+        if n~=m
+            if algorithm=='fsl'
+                connthreshold=threshold;
+            else
+                connthreshold=threshold*2/(size_roi(n)+size_roi(m));
+
+            end
+            
+            if connectome(n,m)>connthreshold
+                if algorithm=='fsl'
+                    connectome(n,m)=(connectome(n,m)/(5000*sum_volume))*2*mean_volume/(size_roi(n)+size_roi(m)); %Normalized by sizes of rois and number of streamlines
+                    %connectome(n,m)=connectome(n,m)/(5000*sum_volume);%Normalized just by number of streamlines
+                    %connectome(n,m)=(connectome(n,m))*2*mean_volume/(size_roi(n)+size_roi(m));%Normalized just by size of ROIs
+                else
+                    connectome(n,m)=connectome(n,m)*mean_volume/10000000;%Normalized by sizes of rois and number of streamlines
+                    %connectome(n,m)=connectome(n,m)/10000000/(2/(size_roi(n)+size_roi(m)));%Normalized by number of streamlines
+                    %connectome(n,m)=connectome(n,m)*mean_volume;
+                end
+            else 
+                connectome(n,m)=0;
+            end
+        end
+    end
+end
+end
+
