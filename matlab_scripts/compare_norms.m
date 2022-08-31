4bd6af883bfb9a11fd04bc5349bb38487b6d1f4a
Ana Matoso <anamatoso92@gmail.com>
2022-08-31 09:00:22 +0100

    Update compare_norms.m

--- matlab_scripts/compare_norms.m
+++ matlab_scripts/compare_norms.m
@@ -1,9 +1,9 @@
 %% Compare Normalizations
-
-%% Load data from matrices
 clear variables
 close all
 format long
+%% Load data from matrices
+
 dir='/Users/ana/Documents/Ana/universidade/Tese/Code/matlab_scripts/matrix_data';
 dir_roi='/Users/ana/Documents/Ana/universidade/Tese/Code/matlab_scripts/roi_sizes';
 atlas="AAL116"; 
@@ -12,6 +12,42 @@ normalizations=[1 2];
 [allconnectomes,n_conditions,n_people,node_labels,condition_names] = get_data(dir,dir_roi,atlas,threshold,normalizations,false);
 
 clear threshold atlas dir dir_roi
+
+%% Scatter plot and correlation coefficients
+nnodes=length(node_labels);
+for con=1:length(allconnectomes)
+    scatterv=[[],[]];
+    scatterv=zeros((sum(n_people)/2)*((nnodes*nnodes-nnodes)/2),2);
+    idx=1;
+    connectomes=allconnectomes{con};
+    for c=[1 2 5 6]%linspace(1,7,4)
+        for p=1:n_people(c)
+            for i=1:nnodes-1
+                for j=i+1:nnodes
+                    scatterv(idx,:)=[connectomes{c}(i,j,p) connectomes{c+2}(i,j,p)];
+                    idx=idx+1;
+                end
+            end
+        end
+    end
+
+    scattervlog=log10(scatterv);
+    for i=length(scattervlog):-1:1
+        if isinf(scattervlog(i,1)) || isinf(scattervlog(i,2))
+            scattervlog(i,:)=[];
+        end
+    end
+
+    figure('color','w')
+    scatterhist(scattervlog(:,1),scattervlog(:,2), 'NBins',[40,40],'Direction','out',Marker='x'); 
+    xlabel('MRTrix');ylabel('FSL')
+    title("Normalization "+num2str(con))
+    R= corr(scattervlog,"Type","Pearson");
+    disp(" ")
+    disp("Pearson's correlation coefficient N"+num2str(con)+": "+ num2str(R(1,2)))
+end
+
+
 %% Get metrics
 allmetrics=cell(size(allconnectomes));
 version_metrics=2;%  1=nodal metrics, 2=general metrics
