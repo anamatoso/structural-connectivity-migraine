4442677276ee9991d20d8c5fe39ff9e59e16ca0f
Ana Matoso <anamatoso92@gmail.com>
2022-10-08 10:02:47 +0100

    update

--- matlab_scripts/plot_boxplots_both.m
+++ matlab_scripts/plot_boxplots_both.m
@@ -32,20 +32,24 @@ for i=1:nmetrics
     
     yyaxis left
     boxplot([x1 nan(1,100)],[group1 4*ones(1,25) 5*ones(1,25) 6*ones(1,25) 7*ones(1,25)],"Colors","b")%,'Labels',labels1)
+    if log10(max(x1))-log10(min(x1))>2
+        set(gca, 'YScale', 'log')
+    end
     %set(gca, 'YScale', 'log')
-    ylim([0.25 0.45])
+    %ylim([0.25 0.45])
     
     yyaxis right
     boxplot([nan(1,100) x2],[0*ones(1,25) 1*ones(1,25) 2*ones(1,25) 3*ones(1,25) group2],"Colors","r",'Labels',patient_labels)
+    if log10(max(x2))-log10(min(x2))>2
+        set(gca, 'YScale', 'log')
+    end
     %set(gca, 'YScale', 'log')
-    ylim([0.25 0.45])
+    %ylim([0.25 0.45])
     title(metrics_labels(metric),'interpreter', 'none','FontSize',15,'FontWeight','normal','FontName','Arial')
 
-    %if log10(max(x1))-log10(min(x1))>2
-     %   set(gca, 'YScale', 'log')
-    %end
-    Image = getframe(gcf);
-    imwrite(Image.cdata, "/Users/ana/Desktop/"+metrics_labels(metric)+".png");
+
+    %Image = getframe(gcf);
+    %imwrite(Image.cdata, "/Users/ana/Desktop/"+metrics_labels(metric)+".png");
 end
 
 end
