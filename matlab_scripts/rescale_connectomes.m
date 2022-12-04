ff896afe7a3fef151d5af1b121470fc7b5acd46c
Ana Matoso <anamatoso92@gmail.com>
2022-12-04 12:20:15 +0000

    correct

--- matlab_scripts/rescale_connectomes.m
+++ matlab_scripts/rescale_connectomes.m
@@ -1,6 +1,14 @@
-function [c] = rescale_connectomes(connectomes,npeople)
+function [c] = rescale_connectomes(connectomes)
 % This function resclales all connectomes to [0,1]
 
+% Determine people per group
+npeople=zeros(size(connectomes));
+for i=1:numel(connectomes)
+    [~,~,n]=size(connectomes{i});
+    npeople(i)=n;
+end
+
+% Rescale every connectome
 c=cell(size(connectomes));
 for i=1:length(connectomes)
     matrices=connectomes{i};
