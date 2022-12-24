87841c6248432495b3813201e6b415e72bf49826
Ana Matoso <anamatoso92@gmail.com>
2022-12-24 16:06:13 +0000

    update documentation

--- shell_scripts/FSL_02_tractography_mat3.sh
+++ shell_scripts/FSL_02_tractography_mat3.sh
@@ -3,7 +3,7 @@
 set -e # Stop on errors
 
 display_usage() {
-	echo "This script uses FSL's probtrackx2 to create a connectivity matrix."
+	echo "This script uses FSL's probtrackx2 to create a connectivity matrix. Note that either the MRtrix pipeline needs to be done first or the command for the division of the atlas should be uncommented."
 	echo "Usage: $(basename $0) [Subject and type of session]"
 	echo "t requires 1 argument: the subject directory. Example: sub-control019_ses-midcycle"
 	}
@@ -26,8 +26,8 @@ DIR=$1 #example name: sub-control019_ses-midcycle
 SUBDIR="${MAINDIR}/${DIR}"
 cd $SUBDIR
 
-# divide the atlas into several files each one with one ROI
-#/home/amatoso/tese/divide_atlas.sh "${SUBDIR}/mrtrix_outputs_bvals2/atlas.mif" $SUBDIR -> done in MRtrix pipeline
+# divide the atlas into several files each one with one ROI -> done in MRtrix pipeline
+# ${MAINDIR}/divide_atlas.sh "${SUBDIR}/mrtrix_outputs_bvals2/atlas.mif" $SUBDIR 
 
 ROIS="${MAINDIR}/${DIR}/list_rois.txt"
 
@@ -36,9 +36,11 @@ if [ ! -f "${MAINDIR}/${DIR}/mrtrix_outputs_bvals2/gmwmseed_atlas.nii.gz" ]; the
 	mrconvert "${MAINDIR}/${DIR}/mrtrix_outputs_bvals2/gmwmseed_atlas.mif" "${MAINDIR}/${DIR}/mrtrix_outputs_bvals2/gmwmseed_atlas.nii.gz"
 fi
 
-########################### STEP 3 ###################################
+########################### STEP 2 ###################################
 #                        Tractography                                #
 ######################################################################
+
+# Set parameters
 n_fibers=5000 		# number of fibers
 fiber_len=250 		# length of fiber in mm
 step=1 				# step of each guess
@@ -47,6 +49,7 @@ seed="${SUBDIR}/bedpostdata.bedpostX/merged"
 output_dir="${SUBDIR}/probtrackX_outputs_omat3"
 gmwm_mask="${MAINDIR}/${DIR}/mrtrix_outputs_bvals2/gmwmseed_atlas.nii.gz"
 
+# Run tractography
 probtrackx2 --network -x $ROIS --usef=0.05 -l --onewaycondition --omatrix3 --target3=$ROIS -c 0.2 -S $fiber_len --steplength=$step --stop=$gmwm_mask -P $n_fibers --fibthresh=0.01 --distthresh=4 --sampvox=0.0 --forcedir --opd -s $seed -m $mask --dir=$output_dir
 
 # copy connectivity matrix to matrices folder
