8a621948cc7072d7c56020e1eadeba89b368a1c3
Ana Matoso <anamatoso92@gmail.com>
2023-04-21 17:14:58 +0100

    add normalization by a null model

--- matlab_scripts/calculate_metrics_v2.m
+++ matlab_scripts/calculate_metrics_v2.m
@@ -14,31 +14,38 @@ if v==2
     [Lrand,GErand]=charpath(d_randmat,0,0);  
     
     Ci=clustering_coef_wu(weight_conversion(mat, 'normalize'));     % local clustering coefficient 
-    C=mean(Ci);                                                     % global clustering coefficient 3    
+    C=mean(Ci);     
+    Cirand=clustering_coef_wu(weight_conversion(randmat, 'normalize'));     % local clustering coefficient 
+    Crand=mean(Cirand);     
     
     [~, Q]=modularity_und(mat);                                     % modularity 4
-    
+    [~, Qrand]=modularity_und(randmat); 
+
     strength=strengths_und(mat);                                    % node strength 
     mean_strength=mean(strength);                                   % mean strength 5
     strengthrand=strengths_und(randmat);
     mean_strengthrand=mean(strengthrand);
     
     T=transitivity_wu(weight_conversion(mat, 'normalize'));         % transitivity 6
-    S=smallworldness3(mat);                                         % smallworldness 7
+    Trand=transitivity_wu(weight_conversion(randmat, 'normalize'));
+    S=smallworldness(mat);                                         % smallworldness 7
     
     
     % insert metrics in matrix
-    metrics=[L/Lrand GE/GErand C Q mean_strength/mean_strengthrand T S]';
+    metrics=[L/Lrand GE/GErand C/Crand Q/Qrand mean_strength/mean_strengthrand T/Trand S]';
 else
     % metrics
     BC=betweenness_wei(len_mat)'/((n_nodes-1)*(n_nodes-2));         % betweenness centrality 1-116
+    BCrand=betweenness_wei(len_randmat)'/((n_nodes-1)*(n_nodes-2));
     Ci=clustering_coef_wu(weight_conversion(mat, 'normalize'))';    % local clustering coefficient 117-232
+    Cirand=clustering_coef_wu(weight_conversion(randmat, 'normalize'))';
     EC=eigenvector_centrality_und(mat)';                            % eigenvector centrality 233-348
+    ECrand=eigenvector_centrality_und(randmat)';  
     strength=strengths_und(mat);                                    % node strength 464-579
     strengthrand=strengths_und(randmat);
 
     % insert metrics in matrix
-    metrics=[BC Ci EC strength./strengthrand]';
+    metrics=[BC/BCrand Ci/Cirand EC/ECrand strength./strengthrand]';
 
 end
 
