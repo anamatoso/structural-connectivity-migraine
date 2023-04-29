7c2ccd7e45cd3d0d5a6054c4e7f7d427e8b79e2e
Ana Matoso <anamatoso92@gmail.com>
2023-04-29 20:56:01 +0100

    add argument capabilities

--- matlab_scripts/nodes/replaceandrename.py
+++ matlab_scripts/nodes/replaceandrename.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import glob
+import sys
 
 # Select all text files
-list_all = glob.glob('*.txt')
+list_all = glob.glob(sys.argv[1]+'/*.txt')
 
 # Replace suffic .txt with .node
 for filename in list_all:
